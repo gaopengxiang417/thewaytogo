53a65a5b619a504e04d4e6a72238695ca9236005
gaopengxiang417 <gaopengxiang417@163.com>
2013-09-08 14:15:44 +0800

    add some array operate

--- controlstructure/collection.go
+++ controlstructure/collection.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -52,6 +53,322 @@ func main() {
 	var sumarray = [3]float64{33.4, 22.5, 434.21321}
 	result := sum_array(&sumarray)
 	fmt.Println("result:", result)
+
+	//slice
+	slice_test()
+
+	////函数中传递slice
+	var origin_array = [10]int{2, 3, 4, 6}
+	fmt.Println(sum_slice(origin_array[:]))
+
+	create_slice()
+	fmt.Println()
+	//
+	slice_funcion()
+
+	//
+	concat_buffer()
+
+	for_range_test()
+
+	var sumslice = make([]float64, 10)
+	fmt.Println(sum_for_float(sumslice))
+	var sumarraysecond = []float64{23.4}
+	fmt.Println(sum_for_float(sumarraysecond))
+
+	reslice()
+
+	var firstslice = []int{2, 4, 5}
+	var secondslce = []int{5, 6, 7}
+	var result_slice = copy_slice(firstslice, secondslce)
+	for _, value := range result_slice {
+		fmt.Printf("%d\t", value)
+	}
+	fmt.Println()
+	fmt.Println(len(result_slice))
+	fmt.Println(cap(result_slice))
+
+	relarge_slice()
+
+	var input = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	ed := filter_slice(input, slice_iseven)
+	for _, value := range ed {
+		fmt.Printf("result is %d\n", value)
+	}
+
+	var a_slice = []int{2, 3, 4, 56}
+	var b_slice = []int{6, 7, 8}
+	insert_slice(a_slice, b_slice, 4)
+	for _, value := range a_slice {
+		fmt.Printf("result %d\t", value)
+	}
+
+	var llsds = []int{1, 2, 3, 4, 5, 6, 7, 8, 89, 9}
+	remove_slice(llsds, 4, 7)
+}
+func remove_slice(a []int, start, end int) {
+	if start > end || start < 0 || end > cap(a) {
+		// do nothing
+		return
+	}
+	//首先应该拷贝一个数组出来,防止越界
+	first := a[:start]
+	second := a[end:]
+
+	var result = make([]int, len(first)+len(second))
+	var count int
+	for index, value := range first {
+		result[index] = value
+		count++
+	}
+	for index, value := range second {
+		result[index+count] = value
+	}
+
+	for _, value := range result {
+		fmt.Printf("%d\t", value)
+	}
+}
+
+//insert 一个slice到另一个的指定位置
+func insert_slice(a []int, b []int, index int) int {
+	if index > cap(a) || index < 0 {
+		return 0
+	}
+	alength := len(a)
+	acapcity := cap(a)
+	blength := len(b)
+
+	if alength+blength > acapcity {
+		var temp = make([]int, (alength+blength)*2)
+		//拷贝
+		for i, value := range a {
+			temp[i] = value
+		}
+		a = temp
+	}
+	//b拷贝到a
+	if index >= alength {
+		for index, value := range b {
+			a[alength+index] = value
+		}
+	} else {
+		ttt := a[index:]
+		for i, value := range b {
+			a[index+i] = value
+		}
+		for i, value := range ttt {
+			fmt.Println("*****", blength, "***", i, "***", index, "***")
+			a[blength+index] = value
+		}
+	}
+	for _, value := range a {
+		fmt.Printf(" %d\t", value)
+	}
+	return blength
+}
+
+//校验函数
+func filter_slice(a []int, fn func(int) bool) []int {
+	var result = make([]int, len(a))
+	for index, value := range a {
+		if fn(value) {
+			result[index] = value
+		}
+	}
+	return result
+}
+
+//是否是偶数
+func slice_iseven(a int) bool {
+	if a%2 == 0 {
+		return true
+	}
+	return false
+}
+
+//relarge slice
+func relarge_slice() {
+	var slice = []int{3, 4, 6}
+	var facotr = 8
+	var result = make([]int, len(slice)*facotr)
+
+	fmt.Println(len(result))
+}
+
+func copy_slice(from []int, to []int) []int {
+	var length = len(from)
+	var capcity = cap(from)
+	if (len(to) + length) > capcity {
+		var temp = make([]int, 2*(len(to)+length))
+		for index, _ := range from {
+			temp[index] = from[index]
+		}
+		from = temp
+	}
+	for index, value := range to {
+		from[length+index] = value
+	}
+	return from
+}
+
+//reslice
+func reslice() {
+	var slice = make([]int, 0, 10)
+	//填充
+	for i := 0; i < cap(slice); i++ {
+		slice = slice[0 : i+1]
+		slice[i] = i * i
+		fmt.Printf("slice index :%d , value is %d,lenth :%d , capcity %d\n", i, slice[i], len(slice), cap(slice))
+	}
+	fmt.Println()
+	//print
+	for index, value := range slice {
+		fmt.Printf("index %d , value %d\n", index, value)
+	}
+
+	var ar = [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var a = ar[5:7]
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+
+	a = a[0:4]
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
+
+	a = a[0:0]
+	fmt.Println(len(a))
+
+}
+
+//slice的计算总和
+func sum_for_float(a []float64) (res float64) {
+	for _, value := range a {
+		res += value
+	}
+	return
+}
+
+//for range for slice
+func for_range_test() {
+	var arr = make([]int, 5)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i * i
+	}
+
+	//for range first
+	for index, value := range arr {
+		fmt.Println("index:", index, ",value:", value)
+	}
+
+	//for range second
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+	//for range third
+	for index := range arr {
+		fmt.Println(index)
+	}
+
+	//not change for range
+	for _, value := range arr {
+		value *= 2
+	}
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+
+	//can change for range
+	for index, value := range arr {
+		arr[index] = value * 2
+	}
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+}
+
+//bytes.Buffer的使用,用来连接多个字符串
+func concat_buffer() {
+	var str = []string{"this", " is a ", " very inter", "sting story"}
+	var buffer bytes.Buffer
+	for _, val := range str {
+		buffer.WriteString(val)
+	}
+	fmt.Println(buffer.String())
+}
+
+//slice测试
+func slice_funcion() {
+	s := make([]byte, 5)
+	fmt.Println("lenth :", len(s))
+	fmt.Println("capcity :", cap(s))
+
+	s = s[2:3]
+	fmt.Println("length :", len(s))
+	fmt.Println("capcity :", cap(s))
+
+	ss := new([]int)
+	fmt.Println(ss)
+
+	s1 := []byte{'a', 'm', 'd', 'k'}
+	s2 := s1[1:3]
+
+	s2[1] = 'b'
+	for _, value := range s1 {
+		fmt.Printf("%c\t", value)
+	}
+}
+
+//slice的创建
+func create_slice() {
+	var slice1 = make([]byte, 10)
+	slice2 := make([]byte, 10)
+	slice3 := make([]byte, 10, 10)
+	slice4 := new([10]byte)[:]
+
+	fmt.Printf("the type %T", slice1)
+	fmt.Printf("the type %T", slice2)
+	fmt.Printf("the type %T", slice3)
+	fmt.Printf("the type %T", slice4)
+}
+
+//函数中传递slice
+func sum_slice(a []int) (res int) {
+	for _, value := range a {
+		res += value
+	}
+	return
+}
+
+//slice的使用,slice底层是由数组构成,他是没有长度限制
+func slice_test() {
+	//首先声明一个数组
+	var array [10]int
+	//声明一个slice
+	var slice1 = array[2:7]
+
+	//初始化数组
+	for index, _ := range array {
+		array[index] = index
+	}
+	//打印数组
+	for index, value := range array {
+		fmt.Printf("array[%d]=%d\n", index, value)
+	}
+	//打印slice
+	for index, value := range slice1 {
+		fmt.Printf("slice[%d]=%d\n", index, value)
+	}
+
+	//长度
+	fmt.Printf("the length of array is :%d\n", len(array))
+	fmt.Printf("the length of slice is :%d\n", len(slice1))
+	fmt.Printf("the capcity of slice is :%d\n", cap(slice1))
+
+	//添加slice的capcity
+	slice1 = slice1[0:4]
+	fmt.Printf("after slice length:%d\n", len(slice1))
+	fmt.Printf("after slice capcity:%d\n", cap(slice1))
 }
 
 //数组的引用传递
